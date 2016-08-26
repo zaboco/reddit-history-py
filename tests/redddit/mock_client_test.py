8fd38bf648bcd5a1fbd2f2a0182d270b365b7b12
Bogdan Zaharia <zaboco@gmail.com>
2016-08-26 12:12:10 +0300

    add MockRedditClient#add_submission

--- tests/redddit/mock_client_test.py
+++ tests/redddit/mock_client_test.py
@@ -4,16 +4,24 @@ from tests.support.mock_reddit_client import MockRedditClient
 class MockRedditClientTest(Base.RedditClientTest):
   @staticmethod
   def make_client():
-    base_submission = {
-      'title': u'sample submission',
-      'author': u'me',
-      'created_utc': 1472158286.0,
-      'score': 10,
-      'permalink': u'/r/subreddit/comments/4zibaf/title/',
-      'subreddit': u'subreddit'
-    }
-    return MockRedditClient([
-      dict(base_submission, name=u'first'),
-      dict(base_submission, name=u'second'),
-      dict(base_submission, name=u'third')
-    ])
+    submissions = map(make_submission, [u'first', u'second', u'third'])
+    return MockRedditClient(submissions)
+
+  def test_adds_submission_in_front(self):
+    new_submission = make_submission(u'new')
+    prev_submissions = self.client.get_submissions()
+    self.client.add_submission(new_submission)
+    new_submissions = self.client.get_submissions()
+    self.assertListEqual(new_submissions, [new_submission] + prev_submissions)
+
+
+def make_submission(name):
+  base_submission = {
+    'title': u'sample submission',
+    'author': u'me',
+    'created_utc': 1472158286.0,
+    'score': 10,
+    'permalink': u'/r/subreddit/comments/4zibaf/title/',
+    'subreddit': u'subreddit'
+  }
+  return dict(base_submission, name=name)
--- tests/support/mock_reddit_client.py
+++ tests/support/mock_reddit_client.py
@@ -4,8 +4,11 @@ class MockRedditClient:
   def __init__(self, submissions):
     self.submissions = submissions
 
-  def get_submissions(self, limit=0, before=None):
+  def get_submissions(self, limit=100, before=None):
     if before:
       return list(takewhile(lambda s: s != before, self.submissions))
     else:
       return self.submissions[:limit]
+
+  def add_submission(self, submission):
+    self.submissions.insert(0, submission)
