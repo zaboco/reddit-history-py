f3f573abd459884e67afec29aad8e2b6256fa745
Bogdan Zaharia <zaboco@gmail.com>
2016-08-26 15:04:51 +0300

    history: on subsequent calls, store only submissions added since last call

--- fetch/reddit_history.py
+++ fetch/reddit_history.py
@@ -4,8 +4,13 @@ from fetch.mappers import map_submission
 class RedditHistory:
   def __init__(self, reddit_client, data_store, start_with):
     self.reddit_client, self.data_store, self.start_with = [reddit_client, data_store, start_with]
+    self.latest_submission = None
 
   def store_latest(self):
-    submissions = self.reddit_client.get_submissions(limit=self.start_with)
+    if (self.latest_submission):
+      submissions = self.reddit_client.get_submissions(before=self.latest_submission)
+    else:
+      submissions = self.reddit_client.get_submissions(limit=self.start_with)
+    self.latest_submission = submissions[0]
     mapped_submissions = map(map_submission, submissions)
     self.data_store.insert_many(mapped_submissions)
--- tests/reddit_history_test.py
+++ tests/reddit_history_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mockito import mock, verify
+from mock import Mock, call
 
 from fetch.mappers import map_submission
 from fetch.reddit_history import RedditHistory
@@ -7,11 +7,31 @@ from tests.support.mock_reddit_client import MockRedditClient
 
 
 class RedditHistoryTest(TestCase):
-  def test_initially_saves_the_predefined_number_of_submissions(self):
-    mock_reddit_client = MockRedditClient([u'first', u'second', u'third'])
-    mock_data_store = mock()
-    start_with = 1
-    [latest_submission] = mock_reddit_client.get_submissions(limit=start_with)
-    history = RedditHistory(reddit_client=mock_reddit_client, data_store=mock_data_store, start_with=start_with)
-    history.store_latest()
-    verify(mock_data_store).insert_many([map_submission(latest_submission)])
+  def setUp(self):
+    pass
+    self.mock_reddit_client = MockRedditClient([u'first', u'second', u'third'])
+    self.mock_data_store = Mock()
+    self.start_with = 1
+    self.history = RedditHistory(
+      reddit_client=self.mock_reddit_client,
+      data_store=self.mock_data_store,
+      start_with=self.start_with)
+
+  def test_initially_stores_the_predefined_number_of_submissions(self):
+    [latest_submission] = self.mock_reddit_client.get_submissions(limit=self.start_with)
+    self.history.store_latest()
+    self.assertInserted([map_submission(latest_submission)])
+
+  def test_stores_only_submissions_added_while_idle(self):
+    self.__store_latest()
+    new_submission = self.mock_reddit_client.add_submission(u'new')
+    self.history.store_latest()
+    self.assertInserted([map_submission(new_submission)])
+
+  def __store_latest(self):
+    self.history.store_latest()
+    self.mock_data_store.reset_mock()
+
+  def assertInserted(self, submissions):
+    expected_calls = [call.insert_many(submissions)]
+    self.assertListEqual(self.mock_data_store.mock_calls, expected_calls)
