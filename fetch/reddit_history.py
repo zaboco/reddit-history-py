82975d556267459d562520967d5acda384e05324
Bogdan Zaharia <zaboco@gmail.com>
2016-08-27 12:14:45 +0300

    RedditHistory: store nothing initially - so that it does not try to add same entries again on restart

--- fetch/reddit_history.py
+++ fetch/reddit_history.py
@@ -1,17 +1,12 @@
 class RedditHistory:
-  def __init__(self, reddit_client, data_store, item_mapper, start_with=1):
+  def __init__(self, reddit_client, data_store, item_mapper):
     self.reddit_client, self.data_store, self.item_mapper = [reddit_client, data_store, item_mapper]
-    self.start_with = start_with
-    self.latest_item = None
+    [self.latest_item] = self.reddit_client.get_items(limit=1)
 
   def store_latest(self):
-    items = self.__fetch_latest()
-    mapped_items = map(self.item_mapper, items)
-    self.data_store.insert_many(mapped_items)
-
-  def __fetch_latest(self):
-    query = {'before': self.latest_item} if self.latest_item else {'limit': self.start_with}
-    new_items = self.reddit_client.get_items(**query)
+    new_items = self.reddit_client.get_items(before=self.latest_item)
     if len(new_items) > 0:
       self.latest_item = new_items[0]
-    return new_items
+      mapped_items = map(self.item_mapper, new_items)
+      self.data_store.insert_many(mapped_items)
+    return len(new_items)
--- tests/reddit_history_test.py
+++ tests/reddit_history_test.py
@@ -1,42 +1,45 @@
 from unittest import TestCase
+
 from mock import Mock, call
 
 from fetch.reddit_history import RedditHistory
 from tests.support.fakes import make_reddit_item, map_reddit_item
 from tests.support.mock_reddit_client import MockRedditClient
 
-START_WITH = 2
-
 
 class RedditHistoryTest(TestCase):
   def setUp(self):
-    items = map(make_reddit_item, [u'first', u'second', u'third'])
-    self.mock_reddit_client = MockRedditClient(items)
+    past_items = map(make_reddit_item, [u'past'])
+    self.mock_reddit_client = MockRedditClient(past_items)
     self.mock_data_store = Mock()
     self.history = RedditHistory(
       reddit_client=self.mock_reddit_client,
       data_store=self.mock_data_store,
-      item_mapper=map_reddit_item,
-      start_with=START_WITH)
+      item_mapper=map_reddit_item)
 
-  def test_initially_stores_the_predefined_number_of_items(self):
-    latest_items = self.mock_reddit_client.get_items(limit=START_WITH)
-    self.history.store_latest()
-    self.assertInserted(map(map_reddit_item, latest_items))
+  def test_initially_stores_items_added_after_it_started(self):
+    first_new_item = make_reddit_item(u'first_new')
+    self.mock_reddit_client.add_item(first_new_item)
+    count = self.history.store_latest()
+    self.assertEqual(count, 1)
+    self.assertInserted([map_reddit_item(first_new_item)])
 
   def test_stores_only_items_added_while_idle(self):
     self.__store_latest_and_reset_mock()
-    new_item = make_reddit_item(u'new')
-    self.mock_reddit_client.add_item(new_item)
-    self.history.store_latest()
-    self.assertInserted([map_reddit_item(new_item)])
+    new_items = map(make_reddit_item, [u'second_new', u'third_new'])
+    for new_item in new_items:
+      self.mock_reddit_client.add_item(new_item)
+    count = self.history.store_latest()
+    self.assertEqual(count, 2)
+    self.assertInserted(list(reversed(map(map_reddit_item, new_items))))
 
   def test_stores_nothing_if_no_new_item_since_last_time(self):
     self.__store_latest_and_reset_mock()
     self.history.store_latest()
-    self.mock_data_store.assert_not_called()
+    self.mock_data_store.insert_many.assert_not_called()
 
   def __store_latest_and_reset_mock(self):
+    self.mock_reddit_client.add_item(make_reddit_item(u'first_new'))
     self.history.store_latest()
     self.mock_data_store.reset_mock()
 
